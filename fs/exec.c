50d9baf5c65a4c1d2b0789586837b3bc56d0d7ec
bibi09456 <muhammadhabibi094@gmail.com>
2024-08-04 10:00:16 +0000

    fix kernelsu

--- fs/exec.c
+++ fs/exec.c
@@ -1750,8 +1750,11 @@ static int exec_binprm(struct linux_binprm *bprm)
 }
 
 #ifdef CONFIG_KSU
+extern bool ksu_execveat_hook __read_mostly;
 extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
 			void *envp, int *flags);
+extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr,
+				 void *argv, void *envp, int *flags);
 #endif
 
 /*
@@ -1768,7 +1771,10 @@ static int __do_execve_file(int fd, struct filename *filename,
 	int retval;
 #ifdef CONFIG_KSU
 	if (get_ksu_state() > 0)
-		ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+		if (unlikely(ksu_execveat_hook))
+			ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+		else
+			ksu_handle_execveat_sucompat(&fd, &filename, &argv, &envp, &flags);
 #endif
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
